922d5366b47447d083c10eaf9abb2f6f40d4c59c
Pete Richardson <pete@peterichardson.com>
2024-06-06 11:05:10 -0700

    Only show ports with ip addresses
    use -a to show all ports
    Also defined HardwarePortList type to group code better

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use regex::Regex;
 use std::collections::HashMap;
 use std::error::Error;
@@ -7,6 +8,14 @@ use tabled::{
     settings::{object::Columns, themes::Colorization, Alignment, Color, Style},
     Table, Tabled,
 };
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about)]
+struct Config {
+    /// show all hw ports, not just ones with ip addresses
+    #[clap(long, short, action)]
+    pub all_ports: bool,
+}
 
 #[derive(Tabled, Default)]
 #[tabled(rename_all = "PascalCase")]
@@ -79,84 +88,108 @@ impl HardwarePort {
     }
 }
 
-fn get_hw_ports(data: &mut Vec<HardwarePort>) -> Result<(), Box<dyn Error>> {
-    let ports = Command::new("networksetup")
-        .arg("-listallhardwareports")
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8(ports.stdout).expect("bad stdout from networksetup command");
-
-    let re = Regex::new(r"Hardware Port: (.*)\nDevice: (.*)\nEthernet Address: (.*)\n\n").unwrap();
-    for caps in re.captures_iter(&stdout) {
-        let portname = caps[1].to_string();
-        let device: String = caps[2].to_string();
-        let mac_address = caps[3].to_string();
-        data.push(HardwarePort::new(portname, device, mac_address))
-    }
-    Ok(())
+struct HardwarePortList {
+    ports: Vec<HardwarePort>,
 }
 
-fn get_service_order() -> HashMap<String, u8> {
-    // Returns a hash mapping port names to service order
-    // e.g.  "en7" -> 0, "en8" -> 1, "WiFi" -> 3
-    // Used to sort ports for printing
-    //
-    // uses the shell command:
-    //    networksetup -listnetworkserviceorder | grep Device
-    //
-    // which has sample output:
-    //      (Hardware Port: Thunderbolt Ethernet Slot 1, Device: en7)
-    //      (Hardware Port: Thunderbolt Ethernet Slot 0, Device: en8)
-    //      (Hardware Port: Thunderbolt Bridge, Device: bridge0)
-    //      (Hardware Port: Wi-Fi, Device: en0)
-    let networksetup_child = Command::new("networksetup")
-        .arg("-listnetworkserviceorder") // with argument `axww`...
-        .stdout(Stdio::piped()) // of which we will pipe the output.
-        .spawn() // Once configured, we actually spawn the command...
-        .unwrap(); // and assert everything went right.
-    let grep_child_one = Command::new("grep")
-        .arg("Device")
-        .stdin(Stdio::from(networksetup_child.stdout.unwrap())) // Pipe through.
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    let output = grep_child_one.wait_with_output().unwrap();
-    let result = str::from_utf8(&output.stdout).unwrap();
+impl HardwarePortList {
+    fn new() -> Self {
+        let mut port_data: Vec<HardwarePort> = Vec::new();
+        let ports = Command::new("networksetup")
+            .arg("-listallhardwareports")
+            .output()
+            .unwrap();
+        let stdout = String::from_utf8(ports.stdout).expect("bad stdout from networksetup command");
 
-    //println!("{}", result);
-    let mut service_order: HashMap<String, u8> = HashMap::new();
-    let mut i = 0;
-    for line in result.lines() {
-        // remove trailing ')'
-        let mut device: &str = line
-            .strip_suffix(|_: char| true)
-            .expect("no ) at end of serviceorder line!");
-        device = device
-            .split_ascii_whitespace()
-            .last()
-            .expect("Couldn't split on whitespace?");
-        service_order.insert(device.to_string(), i);
-        i += 1;
+        let re =
+            Regex::new(r"Hardware Port: (.*)\nDevice: (.*)\nEthernet Address: (.*)\n\n").unwrap();
+        for caps in re.captures_iter(&stdout) {
+            let portname = caps[1].to_string();
+            let device: String = caps[2].to_string();
+            let mac_address = caps[3].to_string();
+            port_data.push(HardwarePort::new(portname, device, mac_address))
+        }
+
+        //HardwarePortList::sort_by_service_order(&mut port_data);
+        Self { ports: port_data }
     }
 
-    service_order
-}
+    fn in_service_order(mut self) -> Self {
+        fn get_service_order() -> HashMap<String, u8> {
+            // Returns a hash mapping port names to service order
+            // e.g.  "en7" -> 0, "en8" -> 1, "WiFi" -> 3
+            // Used to sort ports for printing
+            //
+            // uses the shell command:
+            //    networksetup -listnetworkserviceorder | grep Device
+            //
+            // which has sample output:
+            //      (Hardware Port: Thunderbolt Ethernet Slot 1, Device: en7)
+            //      (Hardware Port: Thunderbolt Ethernet Slot 0, Device: en8)
+            //      (Hardware Port: Thunderbolt Bridge, Device: bridge0)
+            //      (Hardware Port: Wi-Fi, Device: en0)
+            let networksetup_child = Command::new("networksetup")
+                .arg("-listnetworkserviceorder") // with argument `axww`...
+                .stdout(Stdio::piped()) // of which we will pipe the output.
+                .spawn() // Once configured, we actually spawn the command...
+                .unwrap(); // and assert everything went right.
+            let grep_child_one = Command::new("grep")
+                .arg("Device")
+                .stdin(Stdio::from(networksetup_child.stdout.unwrap())) // Pipe through.
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap();
+            let output = grep_child_one.wait_with_output().unwrap();
+            let result = str::from_utf8(&output.stdout).unwrap();
 
-fn sort_by_service_order(data: &mut Vec<HardwarePort>) {
-    let services_in_order = get_service_order();
-    for port in &mut *data {
-        if services_in_order.contains_key(&port.device) {
-            port.service_order = services_in_order[&port.device];
+            //println!("{}", result);
+            let mut service_order: HashMap<String, u8> = HashMap::new();
+            let mut i = 0;
+            for line in result.lines() {
+                // remove trailing ')'
+                let mut device: &str = line
+                    .strip_suffix(|_: char| true)
+                    .expect("no ) at end of serviceorder line!");
+                device = device
+                    .split_ascii_whitespace()
+                    .last()
+                    .expect("Couldn't split on whitespace?");
+                service_order.insert(device.to_string(), i);
+                i += 1;
+            }
+
+            service_order
+        }
+
+        let services_in_order = get_service_order();
+        for port in &mut *self.ports {
+            if services_in_order.contains_key(&port.device) {
+                port.service_order = services_in_order[&port.device];
+            } else {
+                port.service_order = 255;
+            }
+        }
+
+        self.ports.sort_by_key(|d1| d1.service_order);
+        self
+    }
+
+    fn filter_ports(self, active_only: bool) -> Self {
+        if active_only {
+            let ports = self
+                .ports
+                .into_iter()
+                .filter(|p| !(p.ip_address).is_empty())
+                .collect();
+            Self { ports: ports }
         } else {
-            port.service_order = 255;
+            self
         }
     }
-
-    data.sort_by_key(|d1| d1.service_order);
 }
 
-fn print_table(data: Vec<HardwarePort>) -> Result<(), Box<dyn Error>> {
-    let mut table = Table::new(data);
+fn print_table(data: HardwarePortList) -> Result<(), Box<dyn Error>> {
+    let mut table = Table::new(data.ports);
     table
         .with(Style::rounded())
         .with(Colorization::columns([
@@ -173,8 +206,10 @@ fn print_table(data: Vec<HardwarePort>) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let mut net_data: Vec<HardwarePort> = Vec::new();
-    get_hw_ports(&mut net_data).expect("Failed to read network data");
-    sort_by_service_order(&mut net_data);
-    print_table(net_data).expect("Failed to output table");
+    let config = Config::parse();
+
+    let hardware_ports = HardwarePortList::new()
+        .in_service_order()
+        .filter_ports(!config.all_ports); // filter to active ports only, unless -all-ports
+    print_table(hardware_ports).expect("Failed to output table");
 }
